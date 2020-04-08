57e6bef13059e33f162aba2ce572134673ab1caa
tsaristbomba <andrexerdna@gmail.com>
2020-04-07 21:45:13 -0300

    contact page netlify settings

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,15 +21,18 @@ export default function Contact() {
             <div style={props}>
                 <div className="container">
                     <h3>Contato</h3><br />
-                    <form name="contact" id="form" method="POST" data-netlify="true" action="">
-                        <input placeholder="Nome" type="text" id="fname" name="name" /><br />
+                    <form name="contact" id="form" method="POST">
                         
-                        <input placeholder="Email" type="email" id="fname" name="email" /><br />
+                        <input placeholder="Nome" type="text" name="name" /><br />
+                        
+                        <input placeholder="Email" type="email" name="email" /><br />
+                        
+                        <input type="hidden" name="form-name" value="contact" />
 
-                        <input className="second-row" placeholder="Assunto" type="text" id="fname" name="fname" /><br />
+                        <input className="second-row" placeholder="Assunto" type="text" name="subject" /><br />
 
-                        <textarea placeholder="Mensagem" type="text" id="fname" name="fname" /><br />
-
+                        <textarea placeholder="Mensagem" type="text" name="message" /><br />
+                        
                         <div data-netlify-recaptcha="true"></div>
                         <button type="submit">Enviar</button>
                     </form>
